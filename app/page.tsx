ba4ea1ac7880b21f046a5ceaa5bd3f6114496be7
labbit-23 <pavan.jhamnani@gmail.com>
2025-07-27 18:31:51 +0530

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Welcome to Labbit</h1>
+      <p>This is the home screen using App Router.</p>
+    </div>
+  );
+}
