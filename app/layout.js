12b8c2549f247793e6a96490b548c099cc26838c
labbit-23 <pavan.jhamnani@gmail.com>
2025-07-28 01:36:38 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,16 @@
+"use client";
+
+import { ChakraProvider } from "@chakra-ui/react";
+import customTheme from "./theme"; // adjust the path if your theme.js is elsewhere
+
 export default function RootLayout({ children }) {
   return (
-    <html>
-      <body>{children}</body>
+    <html lang="en">
+      <body>
+        <ChakraProvider theme={customTheme}>
+          {children}
+        </ChakraProvider>
+      </body>
     </html>
   );
 }
